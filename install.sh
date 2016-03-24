d921ca903a601ea9c8bb9e36f3bc961b4ec22d13
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-24 00:29:08 +0000

    electron

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /home/ubuntu
+cd /home/ubuntu/workspace
 
 sudo apt-get install supervisor xvfb fluxbox x11vnc libnotify-dev unifont
 
-#libdbusmenu-glib4 libdbusmenu-gtk4 libindicator7 fonts-liberation libappindicator1 xdg-utils language-pack-ja-base language-pack-ja
-#sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE=”ja_JP:ja”
-#source /etc/default/locale
-   
+npm install electron-prebuilt -D
+
+cd /home/ubuntu
+
 git clone git://github.com/kanaka/noVNC
 
 # .bashrc の末尾に環境変数DISPLAYを追加
